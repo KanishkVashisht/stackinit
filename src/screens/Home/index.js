4cc5024a623371ce2fa0713a15af58f6e35e5c72
KanishkVashisht <kanishk.vashisht@gmail.com>
2018-05-25 12:55:33 -0400

    Styled skeleton minus courses

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import {Grid} from 'semantic-ui-react';
+import {Grid, List, Image} from 'semantic-ui-react';
 
 
 export default class Home extends React.Component {
@@ -13,24 +13,47 @@ export default class Home extends React.Component {
     return (
     <div>
       <div id="logo-holder">
-        <img id="logo" src=require('../../assets/logo/stackInitLogo.png')/>
+        <div className="content">
+              <img id="logo" src={require('../../assets/logo/stackInitLogo.png')}/>
+        </div>
       </div>
       <Grid>
-        <Grid.Row columns={2}>
-          <Grid.Column id="what">
-                <h2>We know how tough it is to get started with new technologies.</h2>
-                <h3>Blog posts, articles and youtube videos only confuse you more.</h3>
-                <p>Starting today, you can get a high level overview of any topic from an expert right here.</p>
-          </Grid.Column>
-          <Grid.Column id="how">
-              <h2>How does it work?</h2>
-              <ul>
-                <li>Select a topic.</li>
-                <li>Contact the instructor and schedule a time.</li>
-                <li>Speak at the decided time and get a good overview of the technology.</li>
+      <Grid.Row columns={2}>
+            <Grid.Column width={6} id="icon">
+                  <img id="logo" src={require('../../assets/logo/chatting512.png')}/>
+            </Grid.Column>
+            <Grid.Column id="what" width={10}>
+                    <h2>Learning new technologies is <b>tough</b>.</h2>
+                    <h3>Finding the right direction is complicated and confusing.</h3>
+                    <p>Talk to experts with industry experience and understand the first principles of any topic.</p>
+           </Grid.Column>
+       </Grid.Row>
+       <Grid.Row id="how" className="content">
+              <hr className="Three-title" data-content="How StackInit works"/>
+              <ul className="lateral-three-row">
+                <li>
+                    <div className="list-item">
+                          <img src={require('../../assets/logo/select.png')}/>
+                          <h1 id="choose-heading">Choose</h1>
+                          <p>Choose a topic from the list below that you want to start learning</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="list-item">
+                          <img src={require('../../assets/logo/handshake.png')}/>
+                          <h1 id="contact-heading">Contact</h1>
+                          <p>Contact the instructor and schedule a time that works for both of you</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="list-item">
+                          <img src={require('../../assets/logo/communication.png')}/>
+                          <h1 id="learn-heading">Learn</h1>
+                          <p>Learn the right direction to get started with and let them point you to good resources</p>
+                    </div>
+                </li>
               </ul>
-          </Grid.Column>
-        </Grid.Row>
+      </Grid.Row>
       </Grid>
     </div>
     )
