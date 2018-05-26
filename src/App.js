7cfa9c459a02e2d72df4a4d883f01bf1481c5200
Sambhav Anand <sambhav182@gmail.com>
2018-05-26 17:45:53 -0400

    Added gooogle analystics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import ReactGA from 'react-ga';
 
 import Routes from './utilities/Routes';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    ReactGA.initialize('UA-119887379-1');
+    // This just needs to be called once since we have no routes in this case.
+    ReactGA.pageview(window.location.pathname);
+  }
   render() {
     return (
       <Routes/>
